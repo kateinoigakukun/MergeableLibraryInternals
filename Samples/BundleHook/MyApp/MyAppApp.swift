0c64085db938a418b018981d0e1ad46a84cd3fe5
Yuta Saito <kateinoigakukun@gmail.com>
2023-06-26 23:39:33 +0900

    Add BundleHook sample

---
+++ Samples/BundleHook/MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Yuta Saito on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
