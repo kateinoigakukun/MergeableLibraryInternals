8be7a5fec2d1f30cc43403fa62152de2df979869
Yuta Saito <kateinoigakukun@gmail.com>
2023-06-26 23:42:34 +0900

    Add empty MyUI framework

---
+++ Samples/BundleHook/MyUI/MyUI.h
@@ -0,0 +1,18 @@
+//
+//  MyUI.h
+//  MyUI
+//
+//  Created by Yuta Saito on 2023/06/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyUI.
+FOUNDATION_EXPORT double MyUIVersionNumber;
+
+//! Project version string for MyUI.
+FOUNDATION_EXPORT const unsigned char MyUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyUI/PublicHeader.h>
+
+
